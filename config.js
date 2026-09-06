fba13673f94e5881be53aef210dfcdb12939ee55
yoga-cell <hariyonodimas79@gmail.com>
2026-09-07 01:18:54 +0700

    Change default BOT_LANG from 'ID' to 'EN'

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ module.exports = {
   SUDO: process.env.SUDO || '919074692450, 918157993101',
   ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || '',
   TIMEZONE: process.env.TIMEZONE || "Asia/Kolkata",
-  BOT_LANG: process.env.BOT_LANG || 'ID',
+  BOT_LANG: process.env.BOT_LANG || 'EN',
   START_MSG: toBool(process.env.START_MSG || 'true'),
   ERROR_MSG: toBool(process.env.ERROR_MSG || 'true'), 
 
