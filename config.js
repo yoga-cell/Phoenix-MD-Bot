6e118856ab0e7203250b0cb4b0373a9d6da4c277
yoga-cell <hariyonodimas79@gmail.com>
2026-09-07 01:09:51 +0700

    Change PREFIX and MODE defaults in config.js

--- config.js
+++ config.js
@@ -21,8 +21,8 @@ module.exports = {
   ALIVE_DATA: process.env.ALIVE_DATA || '👋 ʜᴇʏ &sender, ɪ ᴍ *ᴘʜᴏᴇɴɪx-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ* ᴀʟɪᴠᴇ ɴᴏᴡ!\n\n📌 ᴛʏᴘᴇ *menu* ᴛᴏ ɢᴇᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ.\n\n*ᴘʟᴀᴛꜰᴏʀᴍ:* &platform\n*ʀᴜɴᴛɪᴍᴇ:* &runtime;https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg',
   AUDIO_DATA: process.env.AUDIO_DATA || 'Phoenix-MD;Abhishek Suresh;https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg',
   BOT_INFO: process.env.BOT_INFO || 'ᴘʜᴏᴇɴɪx-ᴍᴅ;ᴀʙʜɪꜱʜᴇᴋ ꜱᴜʀᴇꜱʜ;919074692450;https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg', 
-  PREFIX: process.env.PREFIX || '.', //Type false to use without prerix
-  MODE: process.env.MODE || 'private', //Type public to change bot mode to public
+  PREFIX: process.env.PREFIX || 'false', //Type false to use without prerix
+  MODE: process.env.MODE || 'public', //Type public to change bot mode to public
   SUDO: process.env.SUDO || '919074692450, 918157993101',
   ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || '',
   TIMEZONE: process.env.TIMEZONE || "Asia/Kolkata",
